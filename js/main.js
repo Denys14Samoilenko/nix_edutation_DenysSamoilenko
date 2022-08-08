559a0e1d0cb3feea9edc52a3cbcb9af67937e6cb
Denys14Samoilenko <d.samoilenko92@gmail.com>
2022-08-08 10:11:29 +0300

    HW_4 complited

---
+++ js/main.js
@@ -0,0 +1,62 @@
+import studentArr from './studentArr.js';
+
+console.log(studentArr);
+
+class Student {
+	static #listOfStudents = [];
+	static #id = 1;
+	id;
+
+	constructor(enrollee) {
+		this.id = Student.#id++;
+		Student.#listOfStudents.push({
+			id: this.id,
+			name: enrollee.name,
+			surname: enrollee.surname,
+			ratingPoint: enrollee.ratingPoint,
+			schoolPoint: enrollee.schoolPoint,
+			course: enrollee.course,
+			isSelfPayment: !enrollee.ratingPoint >= 800,
+		});
+	}
+	getId() {
+		return this.id;
+	}
+	getListOfStudents() {
+		return Student.#listOfStudents;
+	}
+	getNotPay() {
+		let students = Student.#listOfStudents;
+		let bestEnrollee = students.filter(
+			student => student.isSelfPayment === false
+		);
+		return bestEnrollee
+			.sort(function (a, b) {
+				if (a.ratingPoint < b.ratingPoint) {
+					return 1;
+				}
+				if (a.ratingPoint > b.ratingPoint) {
+					return -1;
+				}
+				if (a.ratingPoint === b.ratingPoint) {
+					if (a.schoolPoint < b.schoolPoint) {
+						return 1;
+					}
+					if (a.schoolPoint > b.schoolPoint) {
+						return -1;
+					}
+					return 0;
+				}
+			})
+			.slice(0, 5);
+	}
+}
+
+let student1 = new Student(studentArr[0]);
+studentArr.forEach((student, index) => {
+	if (index > 0) {
+		new Student(student);
+	}
+});
+
+console.log(student1.getNotPay());
